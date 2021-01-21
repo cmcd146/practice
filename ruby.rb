53f55af0bb7dae4c91bad47c5b26e910863e1fb4
Chris McDonald <mcdonaldchristopherkevin@gmail.com>
2021-01-21 17:09:21 -0500

    debugging

--- ruby.rb
+++ ruby.rb
@@ -2,19 +2,27 @@ require 'pry'
 require 'byebug'
 
 def prime?(num)
-  (1..num).each do |idx|
+
+  (2...num).each do |idx|
     if num % idx == 0
       return false
     end
   end
+
+  return true
 end
 
 def primes(num_primes)
   ps = []
   num = 1
   while ps.count < num_primes
-    primes << num if prime?(num)
+    if prime?(num)
+        ps << num
+    end
+    num += 1
   end
+
+  return ps
 end
 
 if __FILE__ == $PROGRAM_NAME
